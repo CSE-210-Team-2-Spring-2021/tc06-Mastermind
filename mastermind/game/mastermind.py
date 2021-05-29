0ba3d52c32912580f5263d4cf1db186dc58a6fd9
Kyle Ames <ame18007@byui.edu>
2021-05-29 11:32:51 -0700

    revised mastermind due to conflict on previous pull request

--- mastermind/game/mastermind.py
+++ mastermind/game/mastermind.py
@@ -1,89 +1,79 @@
 import random
 
-
 class Mastermind:
-    """ This is the designated playing "surface".
+    """This is the designated playing "surface".
 
+    Stereotype:
+    Information holder
+    Service Provider
 
-        Stereotype:
-        Information holder
-
-
-        Attributes:
-            _code_list - list of numbers
-    """
+    Attributes:
+        _code_list - list of numbers"""
 
     def __init__(self):
-        """ Constructor, initializes the variables.
+        """Constructor
+        Args:
+            self - an instance of mastermind"""
 
-            Args:
-                self - an instance of Mastermind
-        """
-
+        # initialize variables
         self._code_list = []
         self._prepare()
+    
+    def _prepare(self):
+        """Generate the secret code and turn it into a list
+        Args
+            self - an instance of mastermind"""
+        code = str(random.randint(1000, 9999))
+        self._code_list = [int(x) for x in str(code)]
 
-    def _prepare(self, player):
-        """ Generate the secret code and turn it into a list
+    def compare(self, guess_list):
+        """Responsible for comparing the guess and code and returning the hint value(list)
+        Args
+            self - an instance of mastermind
+            guess_list - a list with str contained guess
+        Returns
+            hint_value - a list with str contained hint"""
+        guess = guess_list
+        code = self._code_list
+        hint = ["*","*","*","*"]
 
-            Args:
-                self - an instance of Mastermind
-        """
-        name = player.get_name()
-        code = str(random.randint(1000, 10000))
-        guess = "----"
-        hint = "****"
-        self._items[name] = [code, guess, hint]
-
-    def compare(self, code, guess):
-        """ Responsible for comparing the guess with the code and returning the hint value(list)
-
-            Args:
-                self (Mastermind): an instance of Mastermind
-                code (string): The code to compare with
-                guess (string): The guess that made
-
-            Returns:
-            string: A hint with the form of [xxxx]
-        """
-        hint = ""
-        for turn, letter in enumerate(guess):
-            if code[turn] == letter:
-                hint += "x"
-            elif letter in code:
-                hint += "o"
+        for i, number in enumerate(guess):
+            if code[i] == number:
+                hint[i] = "x"
+            elif number in code:
+                hint[i] = "o"
             else:
-                hint += "*"
+                hint[i] = "*"
         return hint
-
-    def display(self, player, turn):
-        """ Using compare return a str output with player guesses and hints.
-
-            Args:
-                self - an instance of Mastermind
-
-            Example of string:
-            -------------------------
-            Player Matt: 9452, XXO*
-            Player John: 5490, OXO*
-            -------------------------
+        
+    def hint(self, players):
+        """Using compare return a str output with player guesses and hints
+		Args
             players -  a list of player objects that will be passed in from director,
-            here for the get_name and get_turn functions
-        """
-        player = player.get_name()
-        guess = turn.get_turn()
-        hint = 0
-        text = "\n-------------------------"
-        text += f"\nPlayer {player}: " + "{}, " * guess + "{}" * hint
-        text += f"\nPlayer {player}: "
-        text = "\n-------------------------"
-        return text
+            here for the get_name and get_move functions
+        Returns
+            output - a str with players, their guesses, and applicable hints"""
+        
+        output = "\n--------------------"
+        for i, name in enumerate(players):
+            player = self.player.get_name()
+            guess = self.player.get_move()
+            hint = self.hint
+            output += (f'Player {player}: ' + '{guess}, ' + '{hint}')
+        output += "\n--------------------"
+         
+        return output
 
-    def is_won(self):
-        """ Responsible for seeing if the guess matches the code, if so return boolean True, otherwise False
 
-            Args:
-                self - an instance of Mastermind
-        """
-        won = ["*"] * len(self._code_list)
-        return self._code_list == won
+    def is_won(self, guess_list):
+        """Responsible for seeing if the guess matches the code, if so return boolean True, 
+        otherwise False
+        Args
+            self - an instance of mastermind
+        Returns 
+            bool - True if guess_list matches _code_list, otherwise False"""
+        
+        if guess_list == self._code_list:
+            return True
+        else:
+            return False
